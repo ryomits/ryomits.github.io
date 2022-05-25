fdf5d3a46546f434b3340b2323f83fc9520ad762
mitsui <mitsui@sobuline.com>
2022-05-25 23:35:03 +0900

    fix

--- lib/api.ts
+++ lib/api.ts
@@ -5,39 +5,39 @@ import matter from "gray-matter"
 const articleDirectory = path.join(process.cwd(), "articles")
 
 export type Article = {
-	name: string
-	date: string
-	title: string
-	description: string | null
+  name: string
+  date: string
+  title: string
+  description: string | null
 }
 
 type ArticleMatterData = {
-	date: Date
-	title: string
-	description: string | null
+  date: Date
+  title: string
+  description: string | null
 }
 
 export function findArticles(): Article[] {
-	return fs.readdirSync(articleDirectory)
-		.map(fileName => {
-			const filePath = path.join(articleDirectory, fileName)
-			const content = fs.readFileSync(filePath)
-			const articleMatterData = matter(content).data as ArticleMatterData
+  return fs.readdirSync(articleDirectory)
+    .map(fileName => {
+      const filePath = path.join(articleDirectory, fileName)
+      const content = fs.readFileSync(filePath)
+      const articleMatterData = matter(content).data as ArticleMatterData
 
-			return {
-				name: fileName,
-				date: articleMatterData.date.toString(),
-				title: articleMatterData.title,
-				description: articleMatterData.description,
-			}
-		})
-		.sort((a, b) => {
-			if (a.name < b.name) {
-				return 1;
-			}
-			if (a.name > b.name) {
-				return -1;
-			}
-			return 0;
-		})
+      return {
+        name: fileName,
+        date: articleMatterData.date.toString(),
+        title: articleMatterData.title,
+        description: articleMatterData.description,
+      }
+    })
+    .sort((a, b) => {
+      if (a.name < b.name) {
+        return 1;
+      }
+      if (a.name > b.name) {
+        return -1;
+      }
+      return 0;
+    })
 }
