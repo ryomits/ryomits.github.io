0f179f235a66f9cb966e68ec1bc31152c67a6fe4
ryomits <ryomits@users.noreply.github.com>
2022-11-05 04:30:15 +0000

    deploy: ccd6aaa594c0f14a8d28a3df533bf82a4961ef30

--- _next/static/E748txrIu0Urj-fLtlqE-/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-6de14576b5ccc105.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],"/articles/[name]":["static/chunks/pages/articles/[name]-e226009034bba18b.js"],sortedPages:["/","/_app","/_error","/articles/[name]"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
