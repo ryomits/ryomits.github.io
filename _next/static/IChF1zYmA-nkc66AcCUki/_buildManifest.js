a2e277ac70cbc8456040fcc87451948ae94a7480
ryomits <ryomits@users.noreply.github.com>
2023-10-11 13:47:05 +0000

    deploy: b56a0f8bb489e16029481c5338a4c23275af1310

--- _next/static/IChF1zYmA-nkc66AcCUki/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-f022e33b4ee014bc.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],"/articles/[name]":["static/chunks/pages/articles/[name]-c4cb51d45b31e46b.js"],sortedPages:["/","/_app","/_error","/articles/[name]"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
