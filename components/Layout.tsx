7785fb58c0b8734e2865da2630e0418d52cc4595
mitsui <mitsui@sobuline.com>
2023-10-13 00:12:49 +0900

    fix

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -8,9 +8,9 @@ type Props = {
 export default function Layout({ children }: Props) {
   return (
     <Box sx={{ backgroundColor: "#2b2d2e", minHeight: "100vh" }}>
-      <PageLayout>
+      <PageLayout sx={{ maxWidth: "50rem", margin: "0 auto", wordWrap: "break-word" }}>
         <PageLayout.Header>
-          <Pagehead sx={{ marginBottom: 0 }}>
+          <Pagehead>
             <Link href="/">ryomits.github.io</Link>
           </Pagehead>
         </PageLayout.Header>
